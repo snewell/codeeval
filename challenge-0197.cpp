219564f1eb9dccc7e67511ae7127c9d2b717bdda
Stephen Newell <sjnewell@gmail.com>
2016-03-05 23:37:03 -0700

    Solve challenge 197

---
+++ challenge-0197.cpp
@@ -0,0 +1,34 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+
+namespace
+{
+    std::string calculateLabel(int val)
+    {
+        std::string ret;
+        ret.reserve(4);     // reserve the max for the challenge
+        while(val != 0)
+        {
+            --val;
+            auto labelChar = (val % 26);
+            ret.insert(std::begin(ret), labelChar + 'A');
+            val /= 26;
+        }
+        return ret;
+    }
+}
+
+int main(int argc, char ** argv)
+{
+    std::ifstream input{argv[1]};
+    int val;
+
+    input >> val;
+    while(input)
+    {
+        std::cout << calculateLabel(val) << '\n';
+        input >> val;
+    }
+    return 0;
+}
