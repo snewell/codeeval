1410c0197f2468c7eb6bfbc390e210bc16939763
Stephen Newell <sjnewell@gmail.com>
2016-03-06 21:59:13 -0700

    Solve challenge 97

---
+++ challenge-0097.cpp
@@ -0,0 +1,40 @@
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+
+namespace
+{
+    void extractName(std::string data, std::string key)
+    {
+        std::istringstream is{key};
+        int val;
+
+        is >> val;
+        while(is)
+        {
+            std::cout << data[val - 1];
+            is >> val;
+        }
+        std::cout << '\n';
+    }
+}
+
+int main(int argc, char ** argv)
+{
+    std::ifstream input{argv[1]};
+    std::string line;
+
+    std::getline(input, line);
+    while(input)
+    {
+        if(!line.empty())
+        {
+            auto it = std::find(std::begin(line), std::end(line), '|');
+            extractName(std::string{std::begin(line), it}, std::string{it + 1, std::end(line)});
+        }
+        std::getline(input, line);
+    }
+    return 0;
+}
