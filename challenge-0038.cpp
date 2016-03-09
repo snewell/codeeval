d87e77d1e303b0e58d5e58ff47efe4bfbb39af0e
Stephen Newell <sjnewell@gmail.com>
2016-03-08 23:06:00 -0700

    Solve challenge 38

---
+++ challenge-0038.cpp
@@ -0,0 +1,71 @@
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <string>
+
+namespace
+{
+    void trimAlphabet(std::string &alphabet)
+    {
+        for(auto i = 0u; i < alphabet.length(); ++i)
+        {
+            auto it = std::begin(alphabet);
+            std::advance(it, i);
+            auto val = *it;
+            std::advance(it, 1);
+            it = std::stable_partition(it, std::end(alphabet), [val] (char ch) {
+                return (val != ch);
+            });
+            alphabet.erase(it, std::end(alphabet));
+        }
+    }
+
+    void buildStringStep(std::size_t size, std::string const &alphabet, std::string &work, std::size_t index, bool first)
+    {
+        if(size != 0)
+        {
+            for(auto i = 0u; i < alphabet.length(); ++i)
+            {
+                work[index] = alphabet[i];
+                buildStringStep(size - 1, alphabet, work, index + 1, first && (i == 0));
+            }
+        }
+        else
+        {
+            if(!first)
+            {
+                std::cout << ',';
+            }
+            else
+            {
+                first = false;
+            }
+            std::cout << work;
+        }
+    }
+
+    void printStrings(std::size_t size, std::string const &alphabet)
+    {
+        std::string work(size, ' ');
+        buildStringStep(size, alphabet, work, 0, true);
+    }
+}
+
+int main(int argc, char ** argv)
+{
+    std::ifstream input{argv[1]};
+    unsigned int size;
+    char ch;
+    std::string alphabet;
+
+    input >> size >> ch >> alphabet;
+    while(input)
+    {
+        std::sort(std::begin(alphabet), std::end(alphabet));
+        trimAlphabet(alphabet);
+        printStrings(size, alphabet);
+        std::cout << '\n';
+        input >> size >> ch >> alphabet;
+    }
+    return 0;
+}
