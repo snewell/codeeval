3561ed59865f1f90cc325a5bc8de577680a20758
Stephen Newell <sjnewell@gmail.com>
2016-03-08 19:41:01 -0700

    Another easy challenge

---
+++ challenge-0018.cpp
@@ -0,0 +1,22 @@
+#include <fstream>
+#include <iostream>
+
+int main(int argc, char ** argv)
+{
+    std::ifstream input{argv[1]};
+    int x;
+    int y;
+    char ch;
+
+    input >> x >> ch >> y;
+    while(input)
+    {
+        auto result = y;
+        while(result < x)
+        {
+            result += y;
+        }
+        std::cout << result << '\n';
+        input >> x >> ch >> y;
+    }
+}
